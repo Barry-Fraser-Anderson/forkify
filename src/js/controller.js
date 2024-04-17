d11db679d96fb4b6159f79203dc9ce7d9813febf
Barry Anderson <barryanderson60@outlook.com>
2024-04-17 15:39:30 +0100

    Error and success messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-import { state, loadRecipe } from './model.js'
+import { state, loadRecipe } from './model.js';
 import recipeView from './views/recipeView.js';
 
 import 'core-js/stable';
@@ -8,19 +8,17 @@ const recipeContainer = document.querySelector('.recipe');
 
 ///////////////////////////////////////
 
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-  
+
     if (!id) return;
     recipeView.renderSpinner();
-    
+
     await loadRecipe(id);
     recipeView.render(state.recipe);
-
   } catch (error) {
-    console.error(error);
+    recipeView.renderError();
   }
 };
 
